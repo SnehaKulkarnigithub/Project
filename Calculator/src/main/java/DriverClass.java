04d724bcd6fe1814b225ce132c0ae960c441b3c1
ajink <ajink@DESKTOP-247VOBQ>
2023-03-25 16:45:32 +0530

    committed code

--- Calculator/src/main/java/DriverClass.java
+++ Calculator/src/main/java/DriverClass.java
@@ -4,6 +4,9 @@ public class DriverClass {
 	{
 		CalculationsClass cc=new CalculationsClass();
 		System.out.println(cc.addition(300,500));
+		System.out.println(cc.substraction(9000, 80000));
+		System.out.println(cc.multiplication(5, 8));
+		System.out.println(cc.division(15, 3));
 	}
 
 
